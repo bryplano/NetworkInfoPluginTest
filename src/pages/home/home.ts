e4cb1d53f70c93650f29cfe30a54c6546f64f9ff
Bryant Plano <bryant+dev@ionic.io>
2019-03-29 10:50:25 -0500

    Adding plugin, wrapper and basic test logic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Network } from '@ionic-native/network';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  currentNetwork: Network;
 
+  constructor(public navController: NavController, private network: Network) {
+    this.currentNetwork = network;
+
+    // Register event listeners
+    document.addEventListener(`offline`, this.onOfflineStatus, false);
+    document.addEventListener(`online`, this.onOnlineStatus, false);
+  }
+
+  ngOnInit() { }
+
+  onOfflineStatus() {
+    alert('Device is now offline. This means the connection type should be "none".');
+  }
+
+  onOnlineStatus() {
+    alert('Device is now online. See page for connectivity source.');
+  }
+
+  showNetworkConnectionTypeButtonClicked() {
+    alert('Current Network Connection type: ' + this.currentNetwork.type);
   }
 
 }
